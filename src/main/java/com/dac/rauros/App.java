68ad34ce5fca39e3dac5cd596f41fbf62ebb25d1
Diferarce <darenas@vertical.com.co>
2016-12-28 15:33:28 -0500

    Initial Comit

---
+++ src/main/java/com/dac/rauros/App.java
@@ -0,0 +1,13 @@
+package com.dac.rauros;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
